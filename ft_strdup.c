c2d100bf07662fbd62b364c2f0b823267fb4a698
Stella-Kwon <60865769+Stella-Kwon@users.noreply.github.com>
2023-11-27 06:37:41 +0200

    norminette upto ft_strdup.c

--- ft_strdup.c
+++ ft_strdup.c
@@ -3,57 +3,54 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strdup.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: suminkwon <suminkwon@student.42.fr>        +#+  +:+       +#+        */
+/*   By: sukwon <sukwon@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/24 16:40:27 by suminkwon         #+#    #+#             */
-/*   Updated: 2023/11/27 05:52:23 by suminkwon        ###   ########.fr       */
+/*   Updated: 2023/11/27 06:24:09 by sukwon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-char *ft_strdup(const char *str)
+char	*ft_strdup(const char *str)
 {
-    char    *res;
-    int i;
-    
-    res = (char *)malloc(sizeof(str));
-    if(!res || !str)
-        return NULL
-    i = 0;
-    while(str[i] != '\0')
-    {
-        res[i] = str[i];
-        i++;
-    }
-    res[i] = '\0';
-    return (res);
+	char	*res;
+	int		i;
+
+	res = (char *)malloc(sizeof(str));
+	if (!res || !str)
+		return (NULL);
+	i = 0;
+	while (str[i] != '\0')
+	{
+		res[i] = str[i];
+		i++;
+	}
+	res[i] = '\0';
+	return (res);
 }
 
-int main()
+int	main(void)
 {
-    const char *original = "Hello, World!";
+	const char	*original;
+	char		*duplicate;
 
-    // strdup을 사용하여 문자열 복제
-    char *duplicate = strdup(original);
-
-    // 복제된 문자열 사용
-    if (duplicate != NULL)
-    {
-        printf("Original: %s\n", original);
-        printf("Duplicate: %s\n", duplicate);
-    }
-    else
-    {
-        fprintf(stderr, "Memory allocation failed.\n");
-        return 1;
-    }
-
-    // 복제된 문자열의 메모리 해제
-    free(duplicate);
-
-    return 0;
-}
+	original = "Hello, World!";
+	// strdup을 사용하여 문자열 복제
+	duplicate = strdup(original);
+	// 복제된 문자열 사용
+	if (duplicate != NULL)
+	{
+		printf("Original: %s\n", original);
+		printf("Duplicate: %s\n", duplicate);
+	}
+	else
+	{
+		fprintf(stderr, "Memory allocation failed.\n");
+		return (1);
+	}
+	// 복제된 문자열의 메모리 해제
+	free(duplicate);
+	return (0);
