e954c8d133f36b0ab3fc09a0643d92b5d5709e1b
Stella-Kwon <60865769+Stella-Kwon@users.noreply.github.com>
2023-12-08 13:59:42 +0200

    strmapi

---
+++ ft_strmapi.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sukwon <sukwon@student.hive.fi>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/08 12:44:10 by sukwon            #+#    #+#             */
+/*   Updated: 2023/12/08 13:29:51 by sukwon           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdlib.h>
+
+char function(unsigned int i, char c)
+{
+	c += i;
+	return (c);
+}
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	char			*res;
+	unsigned int	i;
+
+	if (!s)
+		return (NULL);
+	i = 0;
+	res = ft_strdup(s);
+	if (!res)
+		return (NULL);
+	while (i < ft_strlen(s))
+	{
+		res[i] = f(i,res[i]);
+		i++;
+	}
+	res[i] = '\0';
+	return (res);
+}
+
+int	main(void)
+{
+	char const *s = "HELLO";
+	printf("%s\n", ft_strmapi(s,function));
+	return (0);
+}
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: sukwon <sukwon@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/03 13:02:16 by suminkwon         #+#    #+#             */
-/*   Updated: 2023/12/07 15:44:24 by sukwon           ###   ########.fr       */
+/*   Updated: 2023/12/08 12:43:59 by sukwon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,4 +41,5 @@ char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
 char	**ft_split(char const *s, char c);
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 #endif
