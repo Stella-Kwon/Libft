38c1104840c163cb33ad01a9d16af0ce5b853183
Su Kwon <skwon2@c2r2p13.hive.fi>
2023-12-12 19:16:24 +0200

    addback

--- ft_lstadd_back_bonus.c
+++ ft_lstadd_back_bonus.c
@@ -6,26 +6,41 @@
 /*   By: skwon2 <skwon2@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/12 15:32:19 by skwon2            #+#    #+#             */
-/*   Updated: 2023/12/12 15:59:59 by skwon2           ###   ########.fr       */
+/*   Updated: 2023/12/12 19:13:31 by skwon2           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void    ft_lstadd_back(t_list **lst, t_list *new)
+void	ft_lstadd_back(t_list **lst, t_list *new)
 {
-    if (!lst || !new) //lst는 **lst의 주소가 NULL인가
-        return ;
-    if (!(*lst)) //lst가 가르키는 주소에 있는 값이 NULL -> lst->next를 말한다. 맨뒤로 보내줘야하기때문에 해준다.
-        *lst = new;
-    else
-    {
-        
-    }
+	t_list	*temp_last;
+
+
+	if (!lst || !new)
+		return ;
+	if (!(*lst))
+		*lst = new;
+	else
+	{
+		temp_last = ft_lstlast(*lst);
+		temp_last -> next = new;
+	}
 }
 
-int main(void)
+int	main(void)
 {
-    
-    return 0;
+	t_list	*lst = ft_lstnew("life");
+	t_list	*new = ft_lstnew("what a ");
+	t_list 	*last = ft_lstnew("!");
+
+	ft_lstadd_front(&lst, new);
+	ft_lstadd_back(&lst, last);
+	while (lst)
+	{
+		printf("%s", lst ->content);
+		lst = lst->next;
+	}
+	printf("\n");
+	return(0);
 }
