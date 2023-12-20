8de7da0c46fb916a9226f3be1bc68d2495f7b7cd
Stella-Kwon <60865769+Stella-Kwon@users.noreply.github.com>
2023-12-20 15:52:04 +0200

    Update ft_lstmap_bonus.c

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -35,5 +35,28 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 		new = new->next;
 		lst = lst->next;
 	}
+	new = NULL;
+	return (head);
+}
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list	*head;
+	t_list	*new;
+	void	*content;
+
+	head = NULL;
+	while (lst)
+	{
+		content = f(lst->content);
+		new = ft_lstnew(content);
+		if (!new)
+		{
+			del(content);
+			ft_lstclear(&head, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&head, new);
+		lst = lst->next;
+	}
 	return (head);
 }
