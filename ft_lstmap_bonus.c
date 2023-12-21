6e33134332bf1bd2baa227afa9e2a2806346ee82
Stella-Kwon <60865769+Stella-Kwon@users.noreply.github.com>
2023-12-21 11:00:03 +0200

    last final

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -6,7 +6,7 @@
 /*   By: sukwon <sukwon@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/14 16:34:14 by sukwon            #+#    #+#             */
-/*   Updated: 2023/12/20 10:29:48 by sukwon           ###   ########.fr       */
+/*   Updated: 2023/12/21 10:59:20 by sukwon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,34 +16,8 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*head;
 	t_list	*new;
-
-	if (!lst || !f || !del)
-		return (NULL);
-	head = ft_lstnew(f(lst->content));
-	if (!head)
-		return (NULL);
-	new = head;
-	lst = lst->next;
-	while (lst)
-	{
-		new->next = ft_lstnew(f(lst->content));
-		if (!new->next)
-		{
-			ft_lstclear(&head, del);
-			return (NULL);
-		}
-		new = new->next;
-		lst = lst->next;
-	}
-	new = NULL;
-	return (head);
-}
-t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
-{
-	t_list	*head;
-	t_list	*new;
 	void	*content;
-	
+
 	if (!lst | !f | !del)
 		return (NULL);
 	head = NULL;
--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,23 +6,25 @@
 /*   By: sukwon <sukwon@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/09 19:02:55 by skwon2            #+#    #+#             */
-/*   Updated: 2023/12/20 10:23:53 by sukwon           ###   ########.fr       */
+/*   Updated: 2023/12/21 10:59:34 by sukwon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <unistd.h>
 
 void	ft_putnbr_fd(int n, int fd)
 {
+	long long	nbr;
+
 	if (fd < 0)
 		return ;
-	if (n < 0)
+	nbr = n;
+	if (nbr < 0)
 	{
 		ft_putchar_fd('-', fd);
-		n *= -1;
+		nbr *= -1;
 	}
-	if (n >= 10)
-		ft_putnbr_fd ((n / 10), fd);
-	ft_putchar_fd((n % 10) + '0', fd);
+	if (nbr >= 10)
+		ft_putnbr_fd ((nbr / 10), fd);
+	ft_putchar_fd((nbr % 10) + '0', fd);
 }
