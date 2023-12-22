e101247fbfd499a33c5333b0311f60c4f45dc048
skwon2 <skwon2@c2r3p8.hive.fi>
2023-12-22 13:55:09 +0200

    the last fixing problem

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: skwon2 <skwon2@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/30 10:48:10 by sukwon            #+#    #+#             */
-/*   Updated: 2023/12/22 13:10:14 by skwon2           ###   ########.fr       */
+/*   Updated: 2023/12/22 13:54:43 by skwon2           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ char	*ft_strjoin(char const *s1, char const *s2)
 		return ((char *)s1);
 	if (!s1 && s2)
 		return ((char *)s2);
-	res = (char *)malloc(sizeof(char) * s1_len + s2_len + 1);
+	res = (char *)malloc(sizeof(char) * (s1_len + s2_len + 1));
 	if (!res)
 		return (NULL);
 	ft_strlcpy(res, s1, s1_len + 1);
